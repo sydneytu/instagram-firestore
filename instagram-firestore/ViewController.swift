13ad5c4a7ea5c46306eb40a920b165fea5fac03c
Sydney Turner <sydney.mariee98@gmail.com>
2022-02-27 19:53:20 -0500

    Initial Commit

---
+++ instagram-firestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-firestore
+//
+//  Created by Sydney Turner on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
